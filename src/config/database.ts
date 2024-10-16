360449e51c36c85db70d517726edc16e2c8fa6cd
raave-aires <raave.aires@proton.me>
2024-10-16 18:02:25 -0300

    checagem da variavel de ambiente

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,9 +4,15 @@ import dotenv from "dotenv";
 dotenv.config(); // carrega as variáveis de ambiente do .env
 mongoose.Promise = global.Promise;
 
+const uri = process.env.MONGODB_URI;
+
 export async function conectarMongoDB() {
+    if(!uri){
+        throw new Error('A variável de ambiente MONGODB_URI não está definida.');
+    }
+    
     try {
-        await mongoose.connect(process.env.MONGODB_URI!);
+        await mongoose.connect(uri);
         console.log('Conectado ao MongoDB!');
     } catch (err) {
         console.error('Erro ao conectar ao MongoDB: ', err);
