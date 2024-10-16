5c4c8cd20e1307c60792562258b17a85af5c9b46
raave-aires <raave.aires@proton.me>
2024-10-16 16:41:51 -0300

    conectado ao mongodb atlas

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 export async function conectarMongoDB() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/orig');
+        await mongoose.connect('mongodb+srv://raave:lhn05HOHXs5tEhTa@nevoento.wz6jk.mongodb.net/orig');
         console.log('Conectado ao MongoDB!');
     } catch (err) {
         console.error('Erro ao conectar ao MongoDB: ', err);
--- src/models/contratos.ts
+++ src/models/contratos.ts
@@ -1,13 +1,47 @@
-import mongoose, { Schema, Document, Model } from "mongoose";
+import mongoose, { Schema, Document, Model, CallbackError } from "mongoose";
 
 interface IContrato extends Document {
-    data_de_contrato: string;
-    nome_do_cliente: string;
+  numero_do_contrato: string;
+  data_de_contrato: string;
+  nome_do_cliente: string;
 }
 
 const esquemaDeContrato: Schema<IContrato> = new Schema({
-    data_de_contrato: { type: String, required: true },
-    nome_do_cliente: { type: String, required: true },
+  numero_do_contrato: { type: String, unique: true }, // Garantimos unicidade
+  data_de_contrato: { type: String, required: true },
+  nome_do_cliente: { type: String, required: true },
+});
+
+// Hook para atribuir o número do contrato automaticamente
+esquemaDeContrato.pre<IContrato>('save', async function (next) {
+  const contrato = this;
+  const dataAtual = new Date();
+  const ano = String(dataAtual.getFullYear()).slice(-2); // Últimos dois dígitos do ano
+  const mes = String(dataAtual.getMonth() + 1).padStart(2, '0'); // Mês com dois dígitos
+
+  const prefixo = `${ano}${mes}`; // Formato base do número de contrato: "YYMM"
+
+  try {
+    // Buscar o último contrato criado neste mês
+    const ultimoContrato = await Contrato.findOne({
+      numero_do_contrato: { $regex: `^${prefixo}-` }
+    }).sort({ numero_do_contrato: -1 });
+
+    let proximoNumero = 1;
+
+    if (ultimoContrato) {
+      // Extrair e incrementar o número crescente do contrato anterior
+      const partes = ultimoContrato.numero_do_contrato.split('-');
+      const ultimoIncremento = parseInt(partes[1], 10);
+      proximoNumero = ultimoIncremento + 1;
+    }
+
+    // Atribuir o novo número de contrato no formato YYMM-XXXX
+    contrato.numero_do_contrato = `${prefixo}-${String(proximoNumero).padStart(4, '0')}`;
+    next();
+  } catch (error) {
+    next(error as CallbackError); // Type assertion para evitar erro de tipagem
+  }
 });
 
 const Contrato: Model<IContrato> = mongoose.model<IContrato>('Contrato', esquemaDeContrato);
